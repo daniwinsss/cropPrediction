7b01d2914656626fe9c6cbe992b70c01afea8fee
daniwinsss <dankdani06@gmail.com>
2026-04-05 21:00:40 +0530

    commit

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 import pickle
 from flask import Flask,request,render_template,jsonify
 import numpy as np
@@ -22,4 +23,5 @@ def predict():
     except Exception as e:
         return jsonify({'error': str(e)}), 500
 if __name__ == '__main__':
-    flask_app.run(debug = True)
+    port = int(os.environ.get("PORT", 5000))
+    flask_app.run(host="0.0.0.0", port=port, debug=False)
